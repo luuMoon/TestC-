b96f951fbb40774e4c90e0f145cd988762d724f0
luyue <229078293@qq.com>
2016-12-05 19:19:15 +0800

    value vs reference

--- Assets/Basic/ValueVsReference.cs
+++ Assets/Basic/ValueVsReference.cs
@@ -1,7 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Runtime.InteropServices;
+
+namespace test1
+{
+	namespace test2
+	{
+		class AA
+		{
+		}
+		namespace test3
+		{
+			class B : test1.test2.AA
+			{
+			}
+		}
+	}
+}
+
 
 public class ValueVsReference : MonoBehaviour {
+
+	public readonly int rOnly1 = -1 , rOnly2;
 
 	struct Point1
 	{
@@ -13,6 +34,18 @@ public class ValueVsReference : MonoBehaviour {
 		public int x,y;
 	}
 
+	struct Point3
+	{
+		public int a;
+		public long b;
+		public byte c;
+		public int d;
+	}
+
+	ValueVsReference()
+	{
+		rOnly1 = 1;
+	}
 	void Start () 
 	{
 		
@@ -20,6 +53,7 @@ public class ValueVsReference : MonoBehaviour {
 		Point1 p1 = new Point1();
 		Point1 p2 = p1;
 		p1.x = 1;
+		p1.y = 2;
 		Debug.Log ("<color=yellow>" + p1.x + "</color>");
 		Debug.Log (p2.x);
 
@@ -29,6 +63,33 @@ public class ValueVsReference : MonoBehaviour {
 		p3.x = 1;
 		Debug.Log ("<color=yellow>" + p3.x + "</color>");
 		Debug.Log (p4.x);
+
+		//test null
+		//error: cant use null for valueType
+		//string valueNull = p1 == null;
+		p3 = null; 	
+		bool referenceNull = p3 == null;
+		//Debug.Log ("<color=yellow>" + p3.x + p3.y + "</color>");
+		Debug.Log(referenceNull);
+
+		//sizeof valueType
+		Point3 p5 = new Point3 ();
+		Debug.Log (Marshal.SizeOf (p5));
+
+		//Test Type
+		Debug.Log (p1.GetType ());
+		Debug.Log (1.GetType ());
+		Debug.Log (0xF11.GetType ());
+		Debug.Log (1.0.GetType ());
+
+		//value must be Defined
+		//int x;
+		//Debug.Log (x);
+
+		//rOnly2 = 1;
+		Debug.Log ("<color=yellow>" + rOnly1 + "</color>" + rOnly2);
+	
+
 	}
 
 }
